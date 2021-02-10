fcb6b74af5fc5b9d854845f52f266385e0a05292
Brandon Y Lam <me@brandonlam.ca>
2021-02-10 10:32:57 -0800

    remove question from index page

--- src/components/QuestionIndexPage.js
+++ src/components/QuestionIndexPage.js
@@ -7,15 +7,28 @@ class QuestionIndexPage extends Component {
     this.state = {
       questions: questionIndexData
     }
+    console.log('Component initialized')
+  }
+
+  deleteQuestion(id) {
+    // anytime you want to change state you must use this.setState
+    this.setState((state) => { // the cb function always recieves the latest state as the argument
+      // return value of this cb function is merged into the state
+      return {
+        questions: state.questions.filter(q => q.id !== id)
+      }
+    })
   }
 
   render() {
+    console.log('Question Index Page Rendered')
     return(
       <main>
         {this.state.questions.map(q => {
           return(
-            <div>
+            <div key={q.id}>
               <h1>{q.id} - {q.title}</h1>
+              <button onClick={() => this.deleteQuestion(q.id)}>Delete</button>
             </div>
           )
         })}
