5ba6671ad07a423e4c44d0b8ad736e4b19a8057a
Brandon Y Lam <me@brandonlam.ca>
2021-02-10 09:03:18 -0800

    move components into modules

--- src/index.js
+++ src/index.js
@@ -3,42 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import QuestionShowPage from './components/QuestionShowPage';
 
-
-const QuestionDetails = () => {
-  return (
-    <div>
-      <h2>What is your favourite colour?</h2>
-      <p>Green, Blue, Magenta, ect.</p>
-      <p>By Jon Snow</p>
-      <p>
-        <small>Seen 10 times</small>
-        <small>Last edited 2 hours</small>
-      </p>
-    </div>
-  )
-}
-
-const AnswerDetails = () => {
-  return (
-    <div>
-      <p>This is my answer</p>
-      <p>By Steve Jobs</p>
-      <p>
-        <strong>Created at:</strong> 1 day ago
-      </p>
-    </div>
-  )
-}
-
-const QuestionShowPage = () => {
-  return(
-    <main>
-      <QuestionDetails/>
-      <AnswerDetails />
-    </main>
-  )
-}
 
 // React.StrictMode is a development only component will give you hints about the compoents that ou write
 ReactDOM.render(
