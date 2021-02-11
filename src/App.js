006a484cf454067cf6e8b2951b332186373a01b8
Brandon Y Lam <me@brandonlam.ca>
2021-02-11 10:01:00 -0800

    add timer

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
 import './App.css';
+import { Component } from 'react';
 import QuestionShowPage from './components/QuestionShowPage';
 import QuestionIndexPage from './components/QuestionIndexPage';
+import CurrentDateTime from './components/CurrentDateTime';
 
-function App() {
-  return (
-    <div className="App">
-      <QuestionShowPage/>
-      <QuestionIndexPage/>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      clocksCount: [1]
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        {
+          this.state.clocksCount.map((c, i) => {
+            return <CurrentDateTime key={i} />
+          })
+        }
+      </div>
+    );
+  }
 }
 
 export default App;
